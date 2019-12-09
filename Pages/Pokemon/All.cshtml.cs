fe264c16a0cbd402d5b5eef6bb6127d7179e61b2
Augustinas Dankevicius <Augustinas.Dankevicius@mitrefinch.co.uk>
2019-12-09 12:36:52 +0000

    initial commit

---
+++ Pages/Pokemon/All.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pokedex.Pages.Pokemon
+{
+    public class AllModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
